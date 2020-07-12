39219ef82f21cede0e09968d8d40894352279b33
heniancheng <845802942@qq.com>
2020-07-12 17:10:07 +0800

    Plots第一次提交：Plots基本使用、属性的使用、以及高级使用的简单介绍。

---
+++ [数据探索Plots]@heniancheng/Plots.mm
@@ -0,0 +1,316 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1594519216077" ID="ID_1557176052" MODIFIED="1594543382294" TEXT="Plots">
+<node CREATED="1593424874840" ID="Freemind_Link_965912763" MODIFIED="1594543226144" POSITION="right" TEXT="&#x521d;&#x8bc6;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1593424893852" ID="Freemind_Link_1587787426" MODIFIED="1593425016740" TEXT="&#x5b83;&#x4ec5;&#x662f;&#x4e00;&#x4e2a;&#x63a5;&#x53e3;&#xff0c;&#x505a;&#x89e3;&#x91ca;&#x5de5;&#x4f5c;&#xff0c;&#x4f7f;&#x7528;&#x5176;&#x4ed6;&#x5e93;&#xff08;&#x540e;&#x7aef;&#xff09;&#x751f;&#x6210;&#x56fe;"/>
+<node CREATED="1593425041569" ID="Freemind_Link_1119633122" MODIFIED="1593425057154" TEXT="&#x540e;&#x7aef;&#x53ef;&#x4ee5;&#x81ea;&#x7531;&#x9009;&#x62e9;"/>
+</node>
+<node CREATED="1593435940663" ID="ID_1912724936" MODIFIED="1594543226149" POSITION="right" TEXT="&#x5b89;&#x88c5;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1593436174818" ID="ID_1559312287" MODIFIED="1594543268913" TEXT="&#x57fa;&#x672c;&#x5b89;&#x88c5;">
+<node CREATED="1593436195504" ID="ID_1020264827" MODIFIED="1593436204677" TEXT="Pkg.add(&quot;Plots&quot;)"/>
+</node>
+<node CREATED="1593423217769" ID="Freemind_Link_971071647" MODIFIED="1594543269823" TEXT="&#x540e;&#x7aef;&#x5305;&#xa;">
+<node CREATED="1593425262570" ID="Freemind_Link_597157486" MODIFIED="1593432961133" TEXT="PyPlot&#xff1a;&#x529f;&#x80fd;&#x591a;&#xff0c;&#x6210;&#x719f;&#xff0c;Python&#x8bed;&#x8a00;"/>
+<node CREATED="1593425269017" ID="Freemind_Link_1469579738" MODIFIED="1593432937443" TEXT="GR&#xff1a;&#x901f;&#x5ea6;&#x5feb;"/>
+<node CREATED="1593432191318" ID="Freemind_Link_655031030" MODIFIED="1593432914700" TEXT="Plotly(JS)&#xff1a;&#x529f;&#x80fd;&#x591a;&#xff0c;&#x6210;&#x719f;&#xff0c;&#x4ea4;&#x4e92;&#x6027;&#x597d;"/>
+<node CREATED="1593432228812" ID="Freemind_Link_1107468898" MODIFIED="1593432996860" TEXT="UnicodePlots&#xff1a;&#x7b80;&#x5355;&#x3001;&#x8f7b;&#x4fbf;"/>
+<node CREATED="1593432262516" ID="Freemind_Link_948833768" MODIFIED="1593433153164" TEXT="InspectDR&#xff1a;&#x4ea4;&#x4e92;&#x6027;&#x597d;&#xff0c;&#x9762;&#x5411;&#x5927;&#x6570;&#x636e;&#x96c6;"/>
+<node CREATED="1593432759083" ID="Freemind_Link_520952716" MODIFIED="1593433047819" TEXT="PGFPlots/PGFPlotsX&#xff1a;Latex&#x652f;&#x6301;"/>
+<node CREATED="1593433310394" ID="Freemind_Link_1614572191" MODIFIED="1593433397492" TEXT="HDF5&#xff1a;&#x7ed8;&#x5236;HDF5&#x683c;&#x5f0f;&#x56fe;&#x7247;"/>
+<node CREATED="1593433208904" ID="Freemind_Link_1482931427" MODIFIED="1593433243390" TEXT="Gadfly&#x3001;Immerse&#xff1a;&#x4e0d;&#x600e;&#x4e48;&#x7528;&#x5566;"/>
+</node>
+<node CREATED="1594432561370" ID="ID_1266180934" MODIFIED="1594543271204" TEXT="&#x73af;&#x5883;&#x914d;&#x7f6e;">
+<node CREATED="1594432572188" ID="ID_176583806" MODIFIED="1594434047752" TEXT="REPL: ENV[&quot;PLOTS_DEFAULT_BACKEND&quot;]=&quot;Plotly&quot;"/>
+<node CREATED="1594432656274" ID="ID_309566093" MODIFIED="1594434050196" TEXT="~/.julia/config/startup.jl: ENV[&quot;PLOTS_DEFAULT_BACKEND&quot;]=&quot;Plotly&quot;"/>
+</node>
+</node>
+<node CREATED="1593435943229" ID="ID_1918952069" MODIFIED="1594543226181" POSITION="right" TEXT="&#x57fa;&#x672c;&#x4f7f;&#x7528;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594432099415" ID="ID_881897604" MODIFIED="1594434284220" TEXT="&#x5bfc;&#x5165;&#x5305;">
+<node CREATED="1594432145089" ID="ID_1303217246" MODIFIED="1594434220640" TEXT="&#x5bfc;&#x5165;Plots">
+<node CREATED="1594432328910" ID="ID_11348858" MODIFIED="1594432330749" TEXT="using Plots"/>
+</node>
+<node CREATED="1594432187549" ID="ID_1746998349" MODIFIED="1594434225775" TEXT="&#x9009;&#x62e9;&#x540e;&#x7aef;">
+<node CREATED="1594432269556" ID="ID_1241542016" MODIFIED="1594432302393" TEXT="gr()"/>
+<node CREATED="1594432290664" ID="ID_1956624486" MODIFIED="1594432299219" TEXT="pyplot()"/>
+<node CREATED="1594432307690" ID="ID_1177677065" MODIFIED="1594432321184" TEXT="plotly()/plotlyjs()"/>
+</node>
+</node>
+<node CREATED="1594434235910" ID="ID_437961743" MODIFIED="1594434282427" TEXT="&#x811a;&#x672c;&#x6267;&#x884c;&#x663e;&#x793a;&#x7ed3;&#x679c;">
+<node CREATED="1594434255324" ID="ID_445248620" MODIFIED="1594434418193" TEXT="default(show=true)"/>
+<node CREATED="1594434369103" ID="ID_955575443" MODIFIED="1594434418764" TEXT="gr(show=true)"/>
+<node CREATED="1594434263576" ID="ID_71954069" MODIFIED="1594434405497" TEXT="display(p)/gui()"/>
+<node CREATED="1594434307642" ID="ID_1623122813" MODIFIED="1594434337870" TEXT="plot(rand(10,1),show=true)"/>
+</node>
+<node CREATED="1593436100921" ID="ID_1688713042" MODIFIED="1594438583160" TEXT="&#x7ebf;&#x6027;&#x56fe;">
+<node CREATED="1593436109977" ID="ID_906134547" MODIFIED="1593436113315" TEXT="&#x5355;&#x7ebf;">
+<node CREATED="1593436168446" ID="ID_1355039506" MODIFIED="1593436170271" TEXT="x=1:10;y=rand(10);plot(x,y)"/>
+</node>
+<node CREATED="1593436113649" ID="ID_625912142" MODIFIED="1594438451887" TEXT="&#x591a;&#x7ebf;">
+<node CREATED="1593436117588" ID="ID_1342537440" MODIFIED="1593436121989" TEXT="&#x5229;&#x7528;&#x77e9;&#x9635;">
+<node CREATED="1593436141567" ID="ID_658685767" MODIFIED="1593436143688" TEXT="x=1:10;y=rand(10,2);plot(x,y)"/>
+</node>
+<node CREATED="1593436122369" ID="ID_557267536" MODIFIED="1593436128705" TEXT="&#x6dfb;&#x52a0;&#x7ebf;&#x6761;">
+<node CREATED="1593436154895" ID="ID_1609095230" MODIFIED="1594434514377" TEXT="x=1:10;z=rand(10);plot!(p,x,z)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1594479943984" ID="ID_904756190" MODIFIED="1594524020786" TEXT="&#x7ed3;&#x679c;&#x4fdd;&#x5b58;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594522231270" ID="ID_108187730" MODIFIED="1594522278220" TEXT="plot(rand(10),fmt=:png)"/>
+<node CREATED="1594438468805" ID="ID_1585799509" MODIFIED="1594522363909" TEXT="savefig(p,&quot;filename&quot;)"/>
+<node CREATED="1594522278685" ID="ID_703764244" MODIFIED="1594522361113" TEXT="png(&quot;filename&quot;)"/>
+</node>
+</node>
+<node CREATED="1593435948151" ID="ID_1264508727" MODIFIED="1594543226186" POSITION="right" TEXT="&#x5c5e;&#x6027;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594523603885" ID="ID_1714191304" MODIFIED="1594543263385" TEXT="&#x57fa;&#x672c;&#x5c5e;&#x6027;&#x4f7f;&#x7528;">
+<node CREATED="1593435953728" ID="ID_1819462199" MODIFIED="1594523638190" TEXT="plot&#x4e2d;&#x521d;&#x59cb;&#x5316;&#x8bbe;&#x7f6e;">
+<node CREATED="1593436023418" HGAP="18" ID="ID_1232091103" MODIFIED="1594523639641" TEXT="&#x7ebf;&#x6761;l" VSHIFT="2">
+<node CREATED="1593521883946" ID="ID_773227191" MODIFIED="1593521920523" TEXT="linestyle&#x3001;linewidth&#x3001;linecolor"/>
+</node>
+<node CREATED="1593521979521" ID="ID_364263494" MODIFIED="1594523643260" TEXT="&#x586b;&#x5145;f">
+<node CREATED="1593521986654" ID="ID_1257849414" MODIFIED="1593521994240" TEXT="fillcolor"/>
+</node>
+<node CREATED="1593436026780" ID="ID_1830988601" MODIFIED="1594436465151" TEXT="&#x6807;&#x9898;">
+<node CREATED="1593436052576" ID="ID_1665040880" MODIFIED="1593436061406" TEXT="title=&quot;Line&quot;"/>
+</node>
+<node CREATED="1593436030298" ID="ID_892061344" MODIFIED="1594436460810" TEXT="&#x56fe;&#x4f8b;">
+<node CREATED="1593436064066" ID="ID_272639156" MODIFIED="1594438132193" TEXT="label=[&quot;Line1&quot; &quot;Line2&quot;]"/>
+</node>
+<node CREATED="1593436033223" ID="ID_237706684" MODIFIED="1594436458291" TEXT="X&#x8f74;">
+<node CREATED="1593436080639" ID="ID_1009356127" MODIFIED="1593436086943" TEXT="xlabel=&quot;X&quot;"/>
+</node>
+<node CREATED="1593436035570" ID="ID_976066956" MODIFIED="1593436037335" TEXT="Y&#x8f74;">
+<node CREATED="1593436089242" ID="ID_1111740448" MODIFIED="1593436097434" TEXT="ylabel=&quot;Y&quot;"/>
+</node>
+<node CREATED="1594436366943" ID="ID_1901411625" MODIFIED="1594437610276" TEXT="X/Y&#x8f74;&#x523b;&#x5ea6;">
+<node CREATED="1594436413561" ID="ID_1719268798" MODIFIED="1594437464356" TEXT="xticks/yticks=&quot;0:1:10&quot;"/>
+</node>
+<node CREATED="1594436793201" ID="ID_1044130033" MODIFIED="1594437609659" TEXT="X/Y&#x8f74;&#x8303;&#x56f4;">
+<node CREATED="1594436826253" ID="ID_1053592846" MODIFIED="1594437442542" TEXT="xlims/ylims=(1,20)"/>
+</node>
+</node>
+<node CREATED="1593435958510" ID="ID_1875243466" MODIFIED="1593521536694" TEXT="&#x540e;&#x671f;&#x66f4;&#x6539;">
+<node CREATED="1593435988107" ID="ID_1515831625" MODIFIED="1594437412660" TEXT="title!(&quot;Line&quot;)"/>
+<node CREATED="1593435990634" ID="ID_1131457304" MODIFIED="1594437397628" TEXT="xlabel!(p,&quot;X&quot;)"/>
+<node CREATED="1593435999955" ID="ID_580490295" MODIFIED="1593436009083" TEXT="ylabel!(p,&quot;Y&quot;)"/>
+<node CREATED="1594437253847" ID="ID_100205767" MODIFIED="1594437403641" TEXT="xticks/yticks!(0:1:10)"/>
+<node CREATED="1594437226303" ID="ID_1354785986" MODIFIED="1594437409172" TEXT="xlims/ylims!(1,10)"/>
+</node>
+</node>
+<node CREATED="1594523197452" ID="ID_643572811" MODIFIED="1594543261169" TEXT="&#x6574;&#x4f53;&#x5c5e;&#x6027;">
+<node CREATED="1594523244397" ID="ID_1829097892" MODIFIED="1594523319761" TEXT="axis/xaxis/yaxis/zaxis&#xa;&#x5305;&#x62ec;xlabel/xlims/xticks&#x7b49;"/>
+<node CREATED="1594523264966" ID="ID_819168738" MODIFIED="1594523431068" TEXT="line&#xa;&#x5305;&#x62ec;linestyle/linewidth/linecolor&#x7b49;"/>
+<node CREATED="1594523355374" ID="ID_1607040774" MODIFIED="1594523463187" TEXT="fill&#xa;&#x5305;&#x62ec;fillrange/fillcolor"/>
+<node CREATED="1594523469546" ID="ID_1824267163" MODIFIED="1594523521535" TEXT="marker&#xa;&#x5305;&#x62ec;markershape/markersize/markercolor&#x7b49;"/>
+</node>
+<node CREATED="1593521537922" ID="ID_355807394" MODIFIED="1594523771521" TEXT="&#x67e5;&#x770b;&#x5e2e;&#x52a9;">
+<node CREATED="1594523666293" ID="ID_645306389" MODIFIED="1594523697619" TEXT="Series&#x5c5e;&#x6027;">
+<node CREATED="1593521560060" ID="ID_428854016" MODIFIED="1593521574032" TEXT="plotattr(:Series)"/>
+</node>
+<node CREATED="1594523700481" ID="ID_17237529" MODIFIED="1594523731054" TEXT="Plots&#x5c5e;&#x6027;">
+<node CREATED="1593521551918" ID="ID_1854421792" MODIFIED="1593521559732" TEXT="plotattr(:Plot)"/>
+</node>
+<node CREATED="1594523731653" ID="ID_1391845431" MODIFIED="1594523742856" TEXT="SubPlot&#x5c5e;&#x6027;">
+<node CREATED="1593521574837" ID="ID_1811651562" MODIFIED="1593521582080" TEXT="plotattr(:Subplot)"/>
+</node>
+<node CREATED="1594523743861" ID="ID_1964943429" MODIFIED="1594523751865" TEXT="Axis&#x5c5e;&#x6027;">
+<node CREATED="1593521582408" ID="ID_1936533560" MODIFIED="1593521590487" TEXT="plotattr(:Axis)"/>
+</node>
+<node CREATED="1593521590981" ID="ID_808652502" MODIFIED="1593521599270" TEXT="plotattr(&quot;size&quot;)"/>
+</node>
+</node>
+<node CREATED="1594476124329" ID="ID_864181736" MODIFIED="1594543244347" POSITION="right" TEXT="&#x9ad8;&#x7ea7;&#x4f7f;&#x7528;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594476167320" ID="ID_232854180" MODIFIED="1594543258458" TEXT="&#x7cfb;&#x5217;&#x56fe;&#xa;plotattr(&quot;seriestype&quot;)">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594479125553" ID="ID_1376704446" MODIFIED="1594479130733" TEXT=" &#x6563;&#x70b9;&#x56fe;">
+<node CREATED="1594542643569" ID="ID_1240838020" MODIFIED="1594542645153" TEXT="scatter(rand(10,2))"/>
+<node CREATED="1594542645578" ID="ID_744176990" MODIFIED="1594542665558" TEXT="plot(rand(10,2),seriestype=:scatter)"/>
+</node>
+<node CREATED="1594542561165" ID="ID_1749671837" MODIFIED="1594543776401" TEXT="&#x6761;&#x5f62;&#x56fe;/&#x67f1;&#x72b6;&#x56fe;/&#x997c;&#x72b6;&#x56fe;">
+<node CREATED="1594542691786" ID="ID_1588930375" MODIFIED="1594543787645" TEXT="bar/histogram/pie"/>
+</node>
+</node>
+<node CREATED="1594476175744" ID="ID_626543425" MODIFIED="1594543255446" TEXT="&#x5e03;&#x5c40;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594476187690" ID="ID_1775432018" MODIFIED="1594529781989" TEXT=" layout=(4,1)">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+</node>
+<node CREATED="1594476207426" ID="ID_7499105" MODIFIED="1594529817117" TEXT="layout = grid(4, 1, heights=[0.1 ,0.4, 0.4, 0.1]))">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+</node>
+<node CREATED="1594529127435" ID="ID_284153365" MODIFIED="1594531424125" TEXT="lo = @layout [a{0.2w] [grid(3,3)&#xa;           b{0.3h}]]&#xa;layout= lo"/>
+</node>
+<node CREATED="1594479496315" ID="ID_308708937" MODIFIED="1594543254182" TEXT="&#x62d3;&#x5c55;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594476951459" ID="ID_1525902311" MODIFIED="1594543720885">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#32479;&#35745;&#32472;&#22270;
+    </p>
+    <p>
+      StatsPlots
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594477243906" ID="ID_115269331" MODIFIED="1594478896536">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DataFrames
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1594477356072" ID="ID_1871473775" MODIFIED="1594477468988">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      df=DataFrame(a=1:10,b=10*rand(10),c=10*rand(10))&#160;
+    </p>
+    <p>
+      @df df plot(:a,[:b :c])
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1594477243906" ID="ID_635359918" MODIFIED="1594531094857">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(34, 34, 34)" face="&#x5b8b;&#x4f53;">Distributions</font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1594477356072" ID="ID_165098080" MODIFIED="1594477907128">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      using Distributions&#160;
+    </p>
+    <p>
+      plot(Normal(3,5),lw=3)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1594477243906" ID="ID_1305067721" MODIFIED="1594478964217">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RDatasets
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1594477356072" ID="ID_1160774711" MODIFIED="1594479031776">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      using RDatasets
+    </p>
+    <p>
+      iris=dataset(&quot;datasets&quot;,&quot;iris&quot;)
+    </p>
+    <p>
+      @df iris marginhist(:PetalLength, :PetalWidth)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1594531581284" ID="ID_68573719" MODIFIED="1594543860960" TEXT="GraphRecipe/GGPlots"/>
+</node>
+<node CREATED="1594479892173" ID="ID_472274308" MODIFIED="1594543252671" TEXT="&#x6570;&#x636e;&#x6e90;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594481478063" ID="ID_139230823" MODIFIED="1594519085132" TEXT=" &#x5411;&#x91cf;&#x6216;&#x77e9;&#x9635;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594481495409" ID="ID_1186165926" MODIFIED="1594519085132" TEXT=" rand(10)/1:10/[1,2,3]">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+</node>
+<node CREATED="1594481502258" ID="ID_1462452681" MODIFIED="1594519085132" TEXT="rand(10,2)/[1 2;3 4;5 6]">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1594520698976" ID="ID_849398664" MODIFIED="1594521483527" TEXT="DataFrames&#x652f;&#x6301;">
+<node CREATED="1594477356072" ID="ID_466077135" MODIFIED="1594477468988">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      df=DataFrame(a=1:10,b=10*rand(10),c=10*rand(10))&#160;
+    </p>
+    <p>
+      @df df plot(:a,[:b :c])
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1594520787983" ID="ID_1725046095" MODIFIED="1594520803480" TEXT="&#x51fd;&#x6570;&#x652f;&#x6301;">
+<node CREATED="1594521498328" ID="ID_1992797969" MODIFIED="1594521653722" TEXT="tvec=range(0, 4&#x3c0;, length = 100)&#xa;plot(sin.(tvec), cos.(tvec))"/>
+</node>
+</node>
+<node CREATED="1594479978675" ID="ID_1993897071" MODIFIED="1594543248190" TEXT="&#x52a8;&#x753b;">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594539688666" ID="ID_209231619" MODIFIED="1594541184856" TEXT="3&#x4e2a;&#x6b65;&#x9aa4;">
+<node CREATED="1594539698782" ID="ID_1806727463" MODIFIED="1594541250609" TEXT="&#x521d;&#x59cb;&#x5316;&#xff08;anim=Animation()&#xff09;"/>
+<node CREATED="1594539722630" ID="ID_911997953" MODIFIED="1594541243902" TEXT="&#x5e27;&#x4fdd;&#x5b58;&#xff08;frame(anim)&#xff09;"/>
+<node CREATED="1594539732838" ID="ID_1079698283" MODIFIED="1594541288233" TEXT="&#x8f6c;&#x5316;&#x6210;gif&#xff08;gif(anim,filename,fps=15&#xff09;"/>
+</node>
+<node CREATED="1594541165131" ID="ID_722810331" MODIFIED="1594541181143" TEXT="&#x5b8f;&#x6307;&#x4ee4;">
+<node CREATED="1594541398301" ID="ID_1918668617" MODIFIED="1594541656036" TEXT="@animate">
+<node CREATED="1594541547729" ID="ID_1003933081" MODIFIED="1594541716643" TEXT="t = range(0, 2&#x3c0;, length = 150);x = sin.(t);y = cos.(t)&#xa;anim = @animate for i &#x2208; 1:n     &#xa;circleplot(x, y, i) end &#xa;gif(anim, &quot;anim_fps15.gif&quot;, fps = 15)"/>
+</node>
+<node CREATED="1594541492623" ID="ID_616219195" MODIFIED="1594541499533" TEXT="@gif">
+<node CREATED="1594541554839" ID="ID_1020357213" MODIFIED="1594541744468" TEXT="t = range(0, 2&#x3c0;, length = 150);x = sin.(t);y = cos.(t)&#xa;@gif for i &#x2208; 1:n     &#xa;circleplot(x, y, i, line_z = 1:n, framestyle = :zerolines) &#xa;end every 5"/>
+</node>
+</node>
+</node>
+<node CREATED="1594479991939" ID="ID_982122568" MODIFIED="1594543250514" TEXT="&#x4e3b;&#x9898;&#xa;PlotThemes">
+<font NAME="&#x5b8b;&#x4f53;" SIZE="12"/>
+<node CREATED="1594539468467" ID="ID_956011098" MODIFIED="1594539487718" TEXT="&#x4e3b;&#x9898;">
+<node CREATED="1594539540998" ID="ID_241019420" MODIFIED="1594539576392" TEXT=":default/:dark/juno/orange&#x7b49;"/>
+</node>
+<node CREATED="1594539494406" ID="ID_880635974" MODIFIED="1594539497583" TEXT="&#x4e3b;&#x9898;&#x9884;&#x89c8;">
+<node CREATED="1594539506209" ID="ID_301272980" MODIFIED="1594539518969" TEXT="showtheme(:default)"/>
+</node>
+<node CREATED="1594539488818" ID="ID_304918438" MODIFIED="1594539494108" TEXT="&#x4e3b;&#x9898;&#x8bbe;&#x7f6e;">
+<node CREATED="1594539530238" ID="ID_1576399896" MODIFIED="1594539539634" TEXT="theme(:default)"/>
+</node>
+</node>
+</node>
+</node>
+</map>
